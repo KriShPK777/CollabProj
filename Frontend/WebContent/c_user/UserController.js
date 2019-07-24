815ebb28eab3918e668b5ceea3b167195387bbab
MR USER 34 <MR USER 34@DESKTOP-RHE8B1A>
2019-07-24 18:47:55 +0530

    Updated Frontend

--- Frontend/WebContent/c_user/UserController.js
+++ Frontend/WebContent/c_user/UserController.js
@@ -1,3 +1,37 @@
-/**
- * 
- */
+myApp.controller("UserController",function($scope,$http,$location,$rootScope,$cookieStore) {	
+       $scope.userDetail=("username":"","password":"","name":"","emailId":"","mobileNo":"","address":"","role":"ROLE_USER");
+       
+       $rootScope.currentUser;
+       		
+	  $scope.register=function()
+	  {
+    	   console.log('I am in Register Function');
+    	   
+    	   $http.post('http://localhost:',$scope.userDetail)
+    	   .then(function(response){
+    		   alert("User Added");
+    	   },function(errresponse){
+    	      alert("Problem Occured");
+    	   }};
+	  }
+
+      $scope.checkuser=function()
+      {
+    	  console.log('I am in checkUser Function');
+    	  console.log($scope.userDetail);
+    	  
+    	  $http.post('http://localhost:',$scope.userDetail)
+    	  .then(function(response)
+    	  {
+    		  $rootScope.currentUser=response.data;
+    		  console.log($rootScope.currentUser);
+    		  $location.path("/userHome");    		  
+        },function(errresponse)
+        {
+        	alert("User Name and Password does not Match");
+        }};
+      }
+
+
+	
+}};
