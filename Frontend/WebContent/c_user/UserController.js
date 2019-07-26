5dda1c956d3db7724a33aceccd5e4eda63f616aa
MR USER 34 <MR USER 34@DESKTOP-RHE8B1A>
2019-07-26 19:50:03 +0530

    Updated Frontend

--- Frontend/WebContent/c_user/UserController.js
+++ Frontend/WebContent/c_user/UserController.js
@@ -1,6 +1,6 @@
 myApp.controller("UserController", function($scope,$http,$location,$rootScope,$cookieStore) {	
        $scope.userDetail=("username":"","password":"","name":"","emailId":"","mobileNo":"","address":"","role":"ROLE_USER");
-       
+       $rootScope.userDetail=("username":"","password":"","name":"","emailId":"","mobileNo":"","address":"","role":"ROLE_USER");
        $rootScope.currentUser;
        		
 	  $scope.register=function()
@@ -15,7 +15,7 @@ myApp.controller("UserController", function($scope,$http,$location,$rootScope,$c
     	   }};
 	  }
 
-      $scope.checkuser=function()
+      $scope.checkUser=function()
       {
     	  console.log('I am in checkUser Function');
     	  console.log($scope.userDetail);
@@ -31,8 +31,45 @@ myApp.controller("UserController", function($scope,$http,$location,$rootScope,$c
         	alert("User Name and Password does not Match");
         }};
       }
+      $scope.editUser=function()
+      {
+    	  console.log('I am in editUser Function');
+       	  
+    	  $http.post('http://localhost:',$rootScope.currentUser.username)
+    	  .then(function(response)
+    	  {
+    		  $rootScope.userDetail=response.data;
+    		  console.log($rootScope.userDetail);
+    		  $location.path("/updateProfile");    		  
+        },function(errresponse)
+        {
+        	alert("Problem Occured");
+        }};
+      }
       
-      
+      $scope.updateProfile=function()
+      {
+    	  console.log('I am in Update Profile Function');
+       	  
+    	  $http.post('http://localhost:',$rootScope.userDetail)
+    	  .then(function(response)
+    	  {
+    		  alert(response.data); 		  
+        },function(errresponse)
+        {
+        	 alert(errorresponse.data); 		
+        	 alert(errorresponse.statusText); 		
+        }};
+      }
+      $scope.logout=function()
+      {
+    	  console.log('I am in Logout Function');
+       	  $cookieStore.remove('userDetail');
+       	  delete $rootScope.currentUser;
+    	
+    		  alert("User has Logged Out"); 
+    		  $location.path("/login");        
+      }
 
 
 	
